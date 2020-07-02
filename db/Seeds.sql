f282f8896c175451f6d96f68b658235ff6e4afac
Annastasshia <59664727+Annastasshia@users.noreply.github.com>
2020-07-01 21:18:15 -0400

    Add files via upload

---
+++ db/Seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name,devoured) VALUES ('Mushroom Swiss Burger, True');
+INSERT INTO burgers (burger_name,devoured) VALUES ('BLT Burger, False');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Veggie Burger, False');
+
+SELECT * FROM Burgers;
